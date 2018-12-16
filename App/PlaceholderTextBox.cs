89443ac1660295b49a9624056477b148dc0860a0
Roman Rieunier <rieunierroman@hotmail.fr>
2018-12-16 17:49:11 +0100

    Création d'un composant Placeholder

---
+++ App/PlaceholderTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    class PlaceholderTextBox : TextBox
+    {
+        public string Placeholder { get; set; }
+
+        protected override void InitLayout()
+        {
+            Text = Placeholder;
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            if (Text == Placeholder)
+                Text = "";
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            if (Text == "")
+                Text = Placeholder;
+        }
+    }
+}
